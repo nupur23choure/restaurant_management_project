e07323ef6fd01ce01953ee97b9ccdf3e4a99b53f
Nupur Choure <90134341+nupur23choure@users.noreply.github.com>
2025-08-05 15:19:41 +0530

    17805

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class MenuModel(models.Model):
+    customer
+    order_item
+    total_amount
+    order_status
+    user
+
