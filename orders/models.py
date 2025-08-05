867eca939fd48e5733bd3524a56fa87b53c0110a
Nupur Choure <90134341+nupur23choure@users.noreply.github.com>
2025-08-05 15:45:39 +0530

    17805

--- orders/models.py
+++ orders/models.py
@@ -13,7 +13,7 @@ class OrderModel(models.Model):
     order_item = models.ManyToManyField(Menu)
     total_amount = models.DecimalField(max_digits = 10, decimal_places=2)
     order_status = models.CharField(max_length=10, choices= STATUS_CHOICES, default='PENDING')
-    create_at = models.DAteTimeField(auto_now_add = True)
+    create_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer.name}"
