b8d4805508722959639fa88db1708ad14a2af24b
Nupur Choure <90134341+nupur23choure@users.noreply.github.com>
2025-08-05 15:35:24 +0530

    17805

--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User
+from account.models import UserProfile
+from products.models import Menu
+
+
 # Create your models here.
 
-class MenuModel(models.Model):
-    customer
-    order_item
-    total_amount
-    order_status
-    user
+class OrderModel(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('CONFIRM', 'Confirm'),
+        ('DELIEVERED', 'Delievered'),
+        ('CANCELLED', 'Cancelled'),
+    ]
+    customer = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    order_item = models.ManyToManyField(Menu)
+    total_amount = models.DecimalField(max_digiti = 10, decimal_places=2)
+    order_status = models.CharField(max_length=10, choices= STATUS_CHOICES, default='PENDING')
+    create_at = models.DAteTimeField(auto_now_add = True)
+
+    defe __str__(self):
+        return f"Order #{self.id} by {self.customer.name}"
 
