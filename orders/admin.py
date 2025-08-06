0c1be8a6e20ff0afc395a265ea2e4f2b1a749f48
Nupur Choure <90134341+nupur23choure@users.noreply.github.com>
2025-08-06 23:52:17 +0530

    17806

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'custoer_name', 'order_date')
+    search_field = ('customer_name',)
+    list_filter = ('order_date' ,)
