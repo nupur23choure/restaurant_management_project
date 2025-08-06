3df5621ade3973f135c23440c8916a3eee95b8cd
Nupur Choure <90134341+nupur23choure@users.noreply.github.com>
2025-08-06 23:37:19 +0530

    17807

--- products/models.py
+++ products/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+class Dish(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return str(self.item_name)
