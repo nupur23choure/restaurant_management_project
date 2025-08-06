927677328856297d49bb8b82c154efa2f87f493b
Nupur Choure <90134341+nupur23choure@users.noreply.github.com>
2025-08-06 23:54:45 +0530

    17806

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,6 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'custoer_name', 'order_date')
+    list_display = ('id', 'customer_name', 'order_date')
     search_field = ('customer_name',)
     list_filter = ('order_date' ,)
